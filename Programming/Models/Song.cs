ff10c0594660a64c4200d2ecc4567f02fa9d69a0
SpivakPavel <spiwakpav@yandex.ru>
2025-02-27 21:41:25 +0700

    lab3.21

--- Programming/Models/Song.cs
+++ Programming/Models/Song.cs
@@ -6,13 +6,73 @@ using System.Threading.Tasks;
 
 namespace Programming.Models
 {
-        internal class Song
+    internal class Song
+    {
+        private string artist;
+        private int duration;
+        private string title;
+        private string album;
+        public Song() { }
+        public string Artist
         {
-            private string artist;
-            private int duration;
-            private string title;
-            private string album;
-            public Song() { }
-            
+            get
+            {
+                return artist;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Значение не может быть пустым или состоять из пробелов.");
+                }
+                artist = value;
+            }
         }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Значение не может быть пустым или состоять из пробелов.");
+                }
+                title = value;
+            }
+        }
+        public string Album
+        {
+            get
+            {
+                return album;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Значение не может быть пустым или состоять из пробелов.");
+                }
+                album = value;
+            }
+        }
+        public int Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Недопустимое значение");
+                }
+                duration = value;
+            }
+        }
+    }
 }
+
--- Programming/Models/Time.cs
+++ Programming/Models/Time.cs
@@ -11,5 +11,51 @@ namespace Programming.Models
         private int hour;
         private int minute;
         private int second;
+
+        public int Hour
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                if (value < 0 || value>23)
+                {
+                    throw new ArgumentException("Недопустимое значение");
+                }
+                hour = value;
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException("Недопустимое значение");
+                }
+                second = value;
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException("Недопустимое значение");
+                }
+                minute = value;
+            }
+        }
     }
 }
