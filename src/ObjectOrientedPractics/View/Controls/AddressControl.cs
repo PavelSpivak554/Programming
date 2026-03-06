e1c9b06a5138848f1ba75f2c25fd8cd5dfe22a8b
SpivakPavel <spiwakpav@yandex.ru>
2026-03-06 17:54:46 +0700

    все файлы

---
+++ src/ObjectOrientedPractics/View/Controls/AddressControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedPractics.View.Tabs
+{
+    public partial class AddressControl : UserControl
+    {
+        public AddressControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddressControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
