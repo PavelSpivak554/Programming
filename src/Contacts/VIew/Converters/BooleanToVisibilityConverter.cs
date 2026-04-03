8660d1361e245fdb44a22a53aa49f9ef768bdb8c
SpivakPavel <spiwakpav@yandex.ru>
2026-04-03 10:39:14 +0700

    feat(Converter) boolToVisibilityConverter added

---
+++ src/Contacts/VIew/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace View.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Конвертор в невидимость кнопки.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Конвертор в видимость кнопки.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible;
+            }
+            return false;
+        }
+    }
+}
